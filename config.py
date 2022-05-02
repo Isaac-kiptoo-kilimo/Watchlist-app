eeb45541e10e6bab4be314c2b5305a752781f288
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-02 23:16:02 +0300

    feature:Deploying news api application

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
+# from distutils.debug import DEBUG
 import os
+
 
 class Config:
 
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:New Password@localhost/watchlist'
 
 
 class ProdConfig(Config):
-    pass
+    DEBUG=False
 
 
 class DevConfig(Config):
